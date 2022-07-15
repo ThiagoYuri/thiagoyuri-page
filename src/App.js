86d1cca487763986f64d7a0897f3e6a729d3fe39
ThiagoYuri <thiagoyuri0109@gmail.com>
2022-07-15 16:38:48 -0300

    Update: Adding NavBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <NavBar></NavBar>
       </header>
     </div>
   );
