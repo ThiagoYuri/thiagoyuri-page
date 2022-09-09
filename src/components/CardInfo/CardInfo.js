230914c3b3fc84b729862413e0585e495ee002e7
ThiagoYuri <thiagoyuri0109@gmail.com>
2022-09-08 22:19:59 -0300

    Update: CardInfo Css

--- src/components/CardInfo/CardInfo.js
+++ src/components/CardInfo/CardInfo.js
@@ -4,15 +4,17 @@ import styles from './CardInfo.module.css';
 
 const CardInfo = () => (
   <div className="card center" >
-    <div id="color-background-img">
-      <div className='center' style={{ height: '70%', width: '70%' }}>
-        <img src={loading} id={styles.imageDirectory} alt="Card projects"/>
+      <div className="btn btn-primary ">
+      <div>
+        <div className='center' style={{ height: '70%', width: '70%' }}>
+          <img src={loading} id={styles.imageDirectory} alt="Card projects"/>
+        </div>
       </div>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <div className="card-text" style={{ height: '30px' }}>card description</div>
+      </div>   
     </div>
-    <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <div className="card-text" style={{ height: '30px' }}>card description</div>
-    </div>   
   </div>
 );
 
