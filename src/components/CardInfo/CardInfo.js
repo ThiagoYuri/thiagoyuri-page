76cc9b065915d47e0af2f14a71664139a3c87e8b
ThiagoYuri <thiagoyuri0109@gmail.com>
2022-09-12 20:51:48 -0300

    Update: CardInfo and Projects

--- src/components/CardInfo/CardInfo.js
+++ src/components/CardInfo/CardInfo.js
@@ -6,13 +6,13 @@ const CardInfo = () => (
   <div className="card center" >
       <div className="btn btn-primary ">
       <div>
-        <div className='center' style={{ height: '70%', width: '70%' }}>
+        <div className='center' style={{ height: '70%', width: '100%' }}>
           <img src={loading} id={styles.imageDirectory} alt="Card projects"/>
         </div>
       </div>
-      <div className="card-body">
+      <div className="card-body" style={{marginBottom:"7%", marginTop:"7%"}}>
         <h5 className="card-title">Card title</h5>
-        <div className="card-text" style={{ height: '30px' }}>card description</div>
+        <p className="card-text" style={{ height: '30px'}} >card description</p>
       </div>   
     </div>
   </div>
--- src/components/Pages/Projects/Projects.js
+++ src/components/Pages/Projects/Projects.js
@@ -5,8 +5,8 @@ const ListComponents = [];
 
 const ListProject = [1,2,3,4]
 ListProject.forEach((data) => {
-    ListComponents.push(<div className="col col-lg-4 col-md-6 col-sm-12 col-xs-12" style={{ paddingTop:'10px'}}>        
-            <CardInfo  />
+    ListComponents.push(<div className="col col-lg-3 col-md-6 col-sm-12 col-xs-12" style={{ paddingTop:'10px'}}>        
+            <CardInfo />
     </div>);
 });
 
