0385d2a10f147d4350c25982f327e27436ccd81a
ThiagoYuri <thiagoyuri0109@gmail.com>
2022-09-20 19:25:26 -0300

    Update: projects card

--- src/components/CardInfo/CardInfo.js
+++ src/components/CardInfo/CardInfo.js
@@ -2,21 +2,24 @@ import React from 'react';
 import loading from '../../resources/loading.gif';
 import styles from './CardInfo.module.css';
 
-const CardInfo = () => (
-  <div className="card center" >
-      <div className="btn btn-primary ">
-      <div>
-        <div className='center' style={{ height: '70%', width: '100%' }}>
-          <img src={loading} id={styles.imageDirectory} alt="Card projects"/>
+function CardInfo (props) {
+    const cardInfo = props.CardInfo
+
+    return (
+      <div className="card">
+          <div className="btn btn-primary ">
+          <div>
+            <div className='card-img-top' >
+              <img src={loading} id={styles.imageDirectory} alt="Card projects"/>
+            </div>
+          </div>
+          <div className="card-body" >
+            <h5 className="card-title" id={styles.titleCenter}>{cardInfo.name.substring(0,30)}</h5>          
+          </div>   
         </div>
       </div>
-      <div className="card-body" style={{marginBottom:"7%", marginTop:"7%"}}>
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text" style={{ height: '30px'}} >card description</p>
-      </div>   
-    </div>
-  </div>
-);
+    )
+};
 
 CardInfo.propTypes = {};
 
--- src/components/Pages/Projects/Projects.js
+++ src/components/Pages/Projects/Projects.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import CardInfo from '../../CardInfo/CardInfo';
+import {get} from '../../../script/api'
+import jsonConfig from '../../../config/config.json'
 
-const ListComponents = [];
 
-const ListProject = [1,2,3,4]
-ListProject.forEach((data) => {
-    ListComponents.push(<div className="col col-lg-3 col-md-6 col-sm-12 col-xs-12" style={{ paddingTop:'10px'}}>        
-            <CardInfo />
-    </div>);
-});
 
-const Projects = () => (  
-      <div className="container">
-          <div className="home-body">
-              <div className="row row-cols-1 row-cols-md-3 g-4" style={{ padding: '0px 20px 0px 20px' }}>
-                  {ListComponents}
-              </div>
-          </div>              
-    </div >
-);
+export function Projects(){
+    
+    const ListComponents = [];
+
+
+
+    const [listRepos,setRepos] = useState([]);
+    useEffect(()=>{
+        get((jsonConfig.GitHub.Link.AllRepos.replace("{0}",jsonConfig.GitHub.Nickname)),setRepos)
+        //setRepos(listRepos)
+    },[])   
+
+
+    listRepos.forEach((data) => {
+        ListComponents.push(<div className="col-12 col-lg-3 col-md-6" style={{ paddingTop:'10px'}}>        
+                <CardInfo CardInfo={data}/>
+        </div>);
+    });
+
+    
+    return(  
+        <div className="container">
+            <div className="home-body">
+                <div className="row row-cols-1 row-cols-md-3 g-4" style={{ padding: '0px 20px 0px 20px' }}>
+                    {ListComponents}
+                </div>
+            </div>              
+        </div >
+    )
+};
 
 Projects.propTypes = {};
 
