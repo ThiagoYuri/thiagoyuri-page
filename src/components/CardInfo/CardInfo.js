3521f28fb45566862806fa4e3451d5f63247dd8d
ThiagoYuri <thiagoyuri0109@gmail.com>
2022-09-23 20:47:56 -0300

    update: logic image project

--- src/components/CardInfo/CardInfo.js
+++ src/components/CardInfo/CardInfo.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import loading from '../../resources/loading.gif';
 import styles from './CardInfo.module.css';
+import jsonConfig from '../../config/config.json'
 
 function CardInfo (props) {
     const cardInfo = props.CardInfo
-
     return (
         <div className="card" id={styles.cardStyle}>
             <div className="btn btn-primary" id={styles.cardStyle}>
             <div>
               <div className='card-img-top' >
-                <img src={loading} id={styles.imageDirectory} alt="Card projects"/>
+                <img 
+                  src={(jsonConfig.Projects.lts[cardInfo.id.toString()]?.image) !== undefined ?require("../../resources/projects/"+jsonConfig.Projects.lts[cardInfo.id.toString()]?.image+".png"):loading}
+                  id={styles.imageDirectory} 
+                  alt="Card projects"/>
               </div>
             </div>
             <div className="card-body" >
