1f253d039e5ca06280b35f80d1fdc5175fd100c9
ThiagoYuri <thiagoyuri0109@gmail.com>
2022-09-26 12:57:06 -0300

    Update: filter logic

--- src/components/Pages/Projects/Projects.js
+++ src/components/Pages/Projects/Projects.js
@@ -4,33 +4,48 @@ import { get } from '../../../script/api'
 import jsonConfig from '../../../config/config.json'
 
 
-export function Projects() {
-    const ListComponents = [];
 
+export function Projects() {
+    var ListComponents = [];
+    const ListCategory = [];
     const [listRepos, setRepos] = useState([]);
+    const [listComponentsFilter, setlistComponentsFilter] = useState([]);
     useEffect(() => {
-        get((jsonConfig.GitHub.Link.AllRepos.replace("{0}", jsonConfig.GitHub.Nickname)), setRepos, (x => x.fork === false))  
+        get((jsonConfig.GitHub.Link.AllRepos.replace("{0}", jsonConfig.GitHub.Nickname)), setRepos, (x => x.fork === false));
+        get((jsonConfig.GitHub.Link.AllRepos.replace("{0}", jsonConfig.GitHub.Nickname)), setlistComponentsFilter, (x => x.fork === false));
     }, [])
 
-
-    listRepos.forEach((data) => {
-        ListComponents.push(<div className="col-12 col-lg-3 col-md-6" style={{ paddingTop: '10px' }}>            
-            <CardInfo CardInfo={data}/>            
+    //filter
+    function filterList() {
+        setlistComponentsFilter(listRepos)
+        if (document.getElementById("searchByName").value !== "")
+            setlistComponentsFilter(listRepos.filter(x => x.name.includes(document.getElementById("searchByName").value)))
+        if (document.getElementById('selectCategory').selectedIndex !== 0){
+            alert(document.getElementById('selectCategory').selectedIndex)
+            setlistComponentsFilter(listRepos.filter(x => jsonConfig.Projects.lts[x.id]?.category === (document.getElementById('selectCategory').selectedIndex-1))
+            )
+        }
+    }
+    //create List
+    listComponentsFilter.forEach((data) => {
+        ListComponents.push(<div className="col-12 col-lg-3 col-md-6" style={{ paddingTop: '10px' }}>
+            <CardInfo CardInfo={data} />
         </div>);
     });
 
+    jsonConfig.Projects.category.forEach((data) => {
+        ListCategory.push(<option>{data}</option>)
+    })
 
     return (
         <div className="container">
             <div className="home-body">
-                <nav className="navbar navbar-expand-lg">           
-                        <input className="form-control" type="search" placeholder="Search by name" aria-label="Search"></input>                        
-                        <select className="form-select " aria-label="Default select example">
-                            <option selected>Select Category</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                        </select>
+                <nav className="navbar navbar-expand-lg">
+                    <input className="form-control" type="search" placeholder="Search by name" aria-label="Search" id='searchByName' onChange={filterList}></input>
+                    <select className="form-select " aria-label="Default select example" id="selectCategory" onChange={filterList}>
+                        <option selected>Select Category</option>
+                        {ListCategory}
+                    </select>
                 </nav>
                 <div className="row row-cols-1 row-cols-md-3 g-4" style={{ padding: '0px 20px 0px 20px' }}>
                     {ListComponents}
