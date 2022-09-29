5a6179add7707b6583f4b2b236e3e8a2ec2e6505
ThiagoYuri <thiagoyuri0109@gmail.com>
2022-09-29 14:22:56 -0300

    Add: footer

--- src/App.js
+++ src/App.js
@@ -39,7 +39,26 @@ function App() {
           <Route path="/Projects" element={<Projects />} />
         </Routes>
       </Router>
-      
+
+      <footer style={{marginTop:"15px"}} className='bg-dark text-white pt-5 pb-4'>
+          <div className='container text-md-left'>
+            <div className='row text-md-left'>             
+              <div className='col-md-3 col-lg-3 col-xl-3 mx-auto mt-3'>
+                <h4 className='text-uppercase mb-4 font-weight-bold text-center text-warning'>Redes Sociais</h4>
+                <ul>
+                  <li><a className='text-white' style={{textDecoration:"none"}} href='#'>Instagram</a></li>
+                  <li><a className='text-white' style={{textDecoration:"none"}} href='#'>Github</a></li>
+                  <li><a className='text-white' style={{textDecoration:"none"}} href='#'>Linkedin</a></li>
+                </ul>
+              </div>
+              <div className='col-md-3 col-lg-3 col-xl-3 mx-auto mt-3'>
+                <h4 className='text-uppercase mb-4 font-weight-bold text-center text-warning'>Contato</h4>
+                <p>Numero: </p>
+                <p>Email: thiagoyuri0109@gmail.com</p>
+              </div>
+            </div>
+          </div>
+      </footer>
     </div>
   );
 }
